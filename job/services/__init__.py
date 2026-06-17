1b097c37c55625e027fc9898592c73de35e5c5b7
ramin <ramindesu88@gmail.com>
2026-06-17 14:21:29 +0330

    FIX(importing problem)

--- job/services/__init__.py
+++ job/services/__init__.py
@@ -1,5 +1,7 @@
+from .create_job import create_job
 from .claim_job import claim_job
 from .complete_job import complete_job
 from .start_job import start_job
 
-__all__ = ["claim_job", "complete_job", "start_job"]
+__all__ = ["create_job", "claim_job", "complete_job", "start_job"]
+
