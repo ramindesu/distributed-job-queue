f8a58ca7192844720bfd407a9b25fda236f83b9d
ramin <ramindesu88@gmail.com>
2026-06-17 15:17:53 +0330

    FIX(claimjob view)

--- job/views/__init__.py
+++ job/views/__init__.py
@@ -1,4 +1,5 @@
 from .create_job import JobCreate
 from .job_detail import JobDetail
+from .claim_job import ClaimJobView
 
-__all__ = ["JobCreate", "JobDetail"]
+__all__ = ["JobCreate", "JobDetail","ClaimJobView"]
