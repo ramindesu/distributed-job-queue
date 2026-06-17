7f6e5209f877e13c9ececb6289d0e965530a987a
ramin <ramindesu88@gmail.com>
2026-06-17 10:28:56 +0330

    UPDATE(django secret and debug)

--- config/settings.py
+++ config/settings.py
@@ -10,20 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
 
+environ.Env.read_env(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g1pup()e4d&z2htw8z$s2hsfzu)8(vbx)gvspih*)fo^+b^1!c'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG' ,default='False')
 
 ALLOWED_HOSTS = []
 
