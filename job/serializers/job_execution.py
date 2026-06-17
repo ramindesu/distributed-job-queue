bb97d542b58723542a629540e5799238933b4d32
ramin <ramindesu88@gmail.com>
2026-06-17 14:20:58 +0330

    FIX(serializer)

--- job/serializers/job_execution.py
+++ job/serializers/job_execution.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..models.job_execute import JobExecution
+
+
 class JobExecutionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JobExecution
-        fields = "__all__"
+        exclude = ["job"]  
