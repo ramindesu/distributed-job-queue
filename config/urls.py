c4772ff2e2962fcb206853bd1527e7cffb9f0810
ramin <ramindesu88@gmail.com>
2026-06-18 23:49:21 +0330

    FIX(url problem)

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
-from job.views import JobCreate, JobDetail
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
