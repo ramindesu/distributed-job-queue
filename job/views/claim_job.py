8bc82e3243604bc4f924be76b8bca4fee59bb3c7
ramin <ramindesu88@gmail.com>
2026-06-18 17:32:19 +0330

    FIX(using get object or 404 for error handling)

--- job/views/claim_job.py
+++ job/views/claim_job.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-from worker.models import Worker
+from django.shortcuts import get_object_or_404
 from services.claim_job import claim_job
 from serializers.job_detail import JobDetailSerializer
+
 
 class ClaimJobView(APIView):
     def post(self, request):
@@ -15,12 +15,12 @@ class ClaimJobView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        worker = Worker.objects.get(pk=worker_id)
+        worker = get_object_or_404(worker, pk=worker_id)
         job = claim_job(worker)
         if not job:
             return Response(
                 {"detail": "No pending jobs available"},
                 status=status.HTTP_404_NOT_FOUND,
             )
-        
+
         return Response(JobDetailSerializer(job).data)
