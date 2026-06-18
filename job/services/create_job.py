786502a6555b62959035fff53a45644c1591e1e0
ramin <ramindesu88@gmail.com>
2026-06-19 01:01:37 +0330

    ADD(create job service for checking idepotency)

--- job/services/create_job.py
+++ job/services/create_job.py
@@ -4,10 +4,28 @@ from job.models.job import Job
 def create_job(type: str, payload: dict, idempotency_key=None) -> tuple[Job, bool]:
 
     if idempotency_key:
-        return Job.objects.get_or_create(
-            idempotency_key=idempotency_key,
-            defaults={"type": type, "payload": payload},
-        )
+
+        from django.db import transaction, IntegrityError
+        
+
+        try:
+            existing_job = Job.objects.get(idempotency_key=idempotency_key)
+            return existing_job, False
+        except Job.DoesNotExist:
+
+            try:
+                with transaction.atomic():
+                    job = Job.objects.create(
+                        idempotency_key=idempotency_key,
+                        type=type,
+                        payload=payload,
+                    )
+                    return job, True
+            except IntegrityError:
+
+
+                existing_job = Job.objects.get(idempotency_key=idempotency_key)
+                return existing_job, False
 
     job = Job.objects.create(type=type, payload=payload)
     return job, True
