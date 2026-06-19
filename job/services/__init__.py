ef66ad5a466ccc90209d9d95763faa93a19779ac
ramin <ramindesu88@gmail.com>
2026-06-19 19:47:21 +0330

    ADD(packing)

--- job/services/__init__.py
+++ job/services/__init__.py
@@ -2,6 +2,16 @@ from .create_job import create_job
 from .claim_job import claim_job
 from .complete_job import complete_job
 from .start_job import start_job
+from .execute_job import execute_job
+from .fail_job import fail_job
+from .process_next_job import process_next_job
 
-__all__ = ["create_job", "claim_job", "complete_job", "start_job"]
-
+__all__ = [
+    "create_job",
+    "claim_job",
+    "complete_job",
+    "start_job",
+    "execute_job",
+    "fail_job",
+    "process_next_job",
+]
