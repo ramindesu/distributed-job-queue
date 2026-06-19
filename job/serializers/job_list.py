afcdb260bb3e6ff566ea3a3e663035852b6c4c19
ramin <ramindesu88@gmail.com>
2026-06-19 23:31:14 +0330

    ADD(worker name)

--- job/serializers/job_list.py
+++ job/serializers/job_list.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
 from ..models.job import Job
 
+
 class JobListSerializer(serializers.ModelSerializer):
+    worker_name = serializers.CharField(source="worker.name", read_only=True)
+
     class Meta:
         model = Job
-        fields = [
-            "id",
-            "type",
-            "status",
-            "created_at",
-        ]
+        fields = ["id", "type", "status", "created_at", "worker_name"]
