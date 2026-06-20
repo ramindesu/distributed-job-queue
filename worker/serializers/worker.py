6281199ca41a981899f9c2a6d45c2ec92db30750
ramin <ramindesu88@gmail.com>
2026-06-20 08:14:10 +0330

    FIX(serializer problem and changing thte database to postgres)

--- worker/serializers/worker.py
+++ worker/serializers/worker.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models.worker import Worker
 class WorkerSerializer(serializers.ModelSerializer):
+    is_alive =serializers.BooleanField(read_only=True)
     class Meta:
-        model: Worker
-        fields = ["__all__"]
+        model=Worker
+        fields = ["id", "name", "status", "last_heartbeat", "created_at", "updated_at",'is_alive']
--- worker/views/worker_list.py
+++ worker/views/worker_list.py
@@ -1,7 +1,6 @@
-from django.urls import path
 from rest_framework.generics import ListAPIView
 from ..models import Worker
-from ..serializers import WorkerSerializer
+from ..serializers.worker import WorkerSerializer
 
 class WorkerListView(ListAPIView):
     queryset = Worker.objects.all().order_by('-last_heartbeat')
